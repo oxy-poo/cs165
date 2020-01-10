1e1ef9700c406db6f1ee6eecfe32acf20d4a8fee
Akshay Gulabrao <agula002@wch132-32.cs.ucr.edu>
2020-01-10 14:54:47 -0800

    Leaving early cuz I feel like it

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "md5.h"
+using namespace std;
+
+int main(){
+
+//team9:$1$4fTgjp6q$nd4.kYa98cdFa3X85Ho8j1:16653:0:99999:7:::
+	string password; // current guess
+	string salt = "4fTgjp6q";
+	string hash = "nd4.kYa98cdFa3X85Ho8j1"; 
+	cout << "Hello" << endl;
+	return 0;
+
+}
